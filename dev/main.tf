fe4483d0d8e3de5ff2c42410e948de773ba1f52e
vasanth-kvn <vasanth-kvn>
2026-08-17 17:48:00 +0530

    1st commit

---
+++ dev/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "s3" {
+    bucket = "5terraform-1234"
+    key = "dev/dev.tfstate"
+    region = var.region
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+
+}
+module "vpc" {
+  source   = "../modules/project-vpc"
+  cidr     = "10.0.0.0/16"
+  vpc-name = "project-vpcaws"
+}
+module "public_subnet" {
+  source            = "./modules/project_subnet"
+  vpc_id            = module.vpc.vpc_id
+  subnet_cidr       = var.subnet_cidr
+  availability_zone = var.availability_zone
+
+}
+module "internet_gateway" {
+  source = "../modules/internet-gateway"
+  vpc_id = module.vpc.vpc_id
+}
+module "route_table" {
+  source    = "../modules/route-table"
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.public_subnet.subnet_id
+  igw   = module.internet_gateway.igw_id
+}
+module "security_group" {
+  source = "../modules/security-group"
+  vpc_id = module.vpc.vpc_id
+}
+module "instance" {
+  source = "../modules/ec2"
+  ami_id            = var.ami_id
+  instance_type     = var.instance_type
+  key_name          = var.key_name
+  subnet_id         = module.public_subnet.subnet_id
+  security_group_id = module.security_group.security_group_id
+}
--- prod/main.tf
+++ prod/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "5terraform-1234"
+    key = "prod/prod.tfstate"
+    region = var.region
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,33 +16,33 @@ provider "aws" {
 
 }
 module "vpc" {
-  source   = "./modules/project-vpc"
+  source   = "../modules/project-vpc"
   cidr     = "10.0.0.0/16"
   vpc-name = "project-vpcaws"
 }
 module "public_subnet" {
-  source            = "./modules/project_subnet"
+  source            = "../modules/project_subnet"
   vpc_id            = module.vpc.vpc_id
   subnet_cidr       = var.subnet_cidr
   availability_zone = var.availability_zone
 
 }
 module "internet_gateway" {
-  source = "./modules/internet-gateway"
+  source = "../modules/internet-gateway"
   vpc_id = module.vpc.vpc_id
 }
 module "route_table" {
-  source    = "./modules/route-table"
+  source    = "../modules/route-table"
   vpc_id    = module.vpc.vpc_id
   subnet_id = module.public_subnet.subnet_id
   igw   = module.internet_gateway.igw_id
 }
 module "security_group" {
-  source = "./modules/security-group"
+  source = "../modules/security-group"
   vpc_id = module.vpc.vpc_id
 }
 module "instance" {
-  source = "./modules/ec2"
+  source = "../modules/ec2"
   ami_id            = var.ami_id
   instance_type     = var.instance_type
   key_name          = var.key_name
